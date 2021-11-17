26718d910cc46c880cf536e04f42f404f1a9b716
Kat <bolivarkat@yahoo.com>
2021-11-17 14:19:41 -0600

    Completed assignmnet

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,29 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useEffect, useState } from "react"
+import ListingCard from "./ListingCard";
 
 function ListingsContainer() {
+  const [listings, setListings] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+    .then((resp) => resp.json())
+    .then((listings) => setListings(listings))
+  }, [])
+
+  function onDeleted(deletedListing){
+    const updatedListings = listings.filter((listing) => listing.id !== deletedListing.id)
+    setListings(updatedListings)
+  }
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listings.map((listing) => (
+          <ListingCard
+          key={listing.id} listing={listing}
+          onDeleted={onDeleted}
+          />
+        ))}
       </ul>
     </main>
   );
